e5c52ceaad543c2c7f27cebbc03b09c75386db35
nehuen-aumedes-diez <nehuen.aumedes@gmail.com>
2022-10-31 14:10:59 -0300

    agregado de react, cambios en el mismo, nueva branch de trabajo, modificacion estetica

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# el-repo
+practica Mern 
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './pngegg.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Wea
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          !Aprender sobre node_modules
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
